18aa072bf4bd947605979f6416561704cf114785
Matyas Lagos <lagosmatyas@gmail.com>
2024-08-06 12:28:00 +0200

    Try some things

--- ecgpack/sp.py
+++ ecgpack/sp.py
@@ -1,10 +1,17 @@
 from ap import txt2list
 from collections import defaultdict
 import random
+import math
 
 # -----
 # Model building algorithms
 # -----
+
+def ngrams_list(sentence, n):
+    beg = ['<s{}>'.format(i) for i in range(1,n)]
+    end = ['</s{}>'.format(i) for i in range(1,n)]
+    sentence = beg + sentence + end
+    return list(zip(*(sentence[i:len(sentence)-n+i+1] for i in range(n))))
 
 def ctxt_dy(corpus, n):
     ngrams = [x for sentence in corpus for x in ngrams_list(sentence, n)]
@@ -31,21 +38,38 @@ def ctxt_dy_aux(ngrams, n):
 
 def envr_dy(corpus, n):
     ngrams = [x for sentence in corpus for x in ngrams_list(sentence, n)]
-    edy = defaultdict(dict)
-    ody = defaultdict(set)
+    edy = {'ctxts': defaultdict(dict), 'argts': defaultdict(dict)}
     for ngram in ngrams:
         while len(ngram) >= 3:
             try:
-                edy[ngram[1:-1]][ngram[:1]][ngram[-1:]] += 1
+                edy['argts'][ngram[1:-1]][ngram[:1] + ngram[-1:]] += 1
             except:
-                edy[ngram[1:-1]][ngram[:1]] = {ngram[-1:]: 1}
-            ody[(ngram[0], ngram[-1])].add(ngram[1:-1])
+                edy['argts'][ngram[1:-1]][ngram[:1] + ngram[-1:]]  = 1
+            try:
+                edy['ctxts'][ngram[:1] + ngram[-1:]][ngram[1:-1]] += 1
+            except:
+                edy['ctxts'][ngram[:1] + ngram[-1:]][ngram[1:-1]]  = 1
             ngram = ngram[:-1]
-    return (edy, ody)
+    for dr in edy:
+        for item in edy[dr]:
+            count = sum(edy[dr][item].values())
+            edy[dr][item]['#'] = count
+    return edy
 
 # -----
 # Neighbour searching algorithms
 # -----
+
+def envr_sims(refr, edy):
+    sims = defaultdict(float)
+    for ctxt in edy['argts'][refr]:
+        if ctxt == '#': continue
+        bw_step = edy['argts'][refr][ctxt] / edy['argts'][refr]['#']
+        for argt in edy['ctxts'][ctxt]:
+            if argt == '#': continue
+            fw_step = edy['ctxts'][ctxt][argt] / edy['ctxts'][ctxt]['#']
+            sims[argt] += bw_step * fw_step
+    return sorted(list(sims.items()), reverse=True, key=lambda x: x[1])
 
 def cmn_envrs(refr, targ, edy):
     envrs = set()
@@ -55,6 +79,24 @@ def cmn_envrs(refr, targ, edy):
                 if fw_ctxt in edy[targ][bw_ctxt]:
                     envrs.add((bw_ctxt[0], fw_ctxt[0]))
     return envrs
+
+def cmn_paths(refr, targ, cdy):
+    anl_ctxt, anl_goal = (refr[0],), (refr[1],)
+    ctxt,     goal     = (targ[0],), (targ[1],)
+    score = 0
+    paths = []
+    for r_item in cmn_fw_nbs(ctxt, anl_ctxt, cdy):
+        bw_step = prob_bw(ctxt, r_item, cdy) * prob_bw(anl_ctxt, r_item, cdy)
+        for l_item in cmn_bw_nbs(goal, anl_goal, cdy):
+            try:
+                bridge = prob_jt(l_item, r_item, cdy)
+                fw_step = prob_fw(l_item, goal, cdy) * prob_fw(l_item, anl_goal, cdy)
+                score += bw_step * bridge * fw_step
+                paths.append((l_item + r_item, score))
+            except:
+                continue
+    print(score)
+    return sorted(paths, key=lambda x: x[1], reverse=True)
 
 def fw_count(ngram, cdy):
     rem_dy = cdy['fw']
@@ -80,7 +122,7 @@ def fw_lookup(ngram, cdy):
         try:
             rem_dy = rem_dy[word]
         except KeyError:
-            continue
+            return {}
     return rem_dy
 
 def bw_lookup(ngram, cdy):
@@ -89,7 +131,7 @@ def bw_lookup(ngram, cdy):
         try:
             rem_dy = rem_dy[word]
         except KeyError:
-            continue
+            return {}
     return rem_dy
 
 def fw_nbs(ngram, cdy, n=float('inf')):
@@ -200,11 +242,167 @@ def nb_gen(srce, trgt, cdy):
             fw_score += prob_fw(srce, fwnb, cdy) * prob_bw(trgt, fwnb, cdy)
         except:
             continue
-    return bw_score * fw_score
+    return math.sqrt(bw_score * fw_score)
+
+def cos_sim(refr, targ, cdy):
+    bw_num = 0
+    bw_ref = 0
+    bw_trg = 0
+    for bwnb in cdy['fw']:
+        if bwnb == '#':
+            continue
+        try:
+            refr_count = fw_lookup((bwnb,) + refr, cdy)['#']
+        except:
+            refr_count = 0
+        try:
+            targ_count = fw_lookup((bwnb,) + targ, cdy)['#']
+        except:
+            targ_count = 0
+        bw_num += refr_count * targ_count
+        bw_ref += refr_count ** 2
+        bw_trg += targ_count ** 2
+    bw_sim = bw_num / (math.sqrt(bw_ref) * math.sqrt(bw_trg))
+    fw_num = 0
+    fw_ref = 0
+    fw_trg = 0
+    for fwnb in cdy['bw']:
+        if fwnb == '#':
+            continue
+        try:
+            refr_count = bw_lookup(refr + (fwnb,), cdy)['#']
+        except:
+            refr_count = 0
+        try:
+            targ_count = bw_lookup(targ + (fwnb,), cdy)['#']
+        except:
+            targ_count = 0
+        fw_num += refr_count * targ_count
+        fw_ref += refr_count ** 2
+        fw_trg += targ_count ** 2
+    fw_sim = fw_num / (math.sqrt(fw_ref) * math.sqrt(fw_trg))
+    return math.sqrt(bw_sim * fw_sim)
+
+def fw_gen_prob(refr, targ, cdy):
+    fw_prob = 0
+    for fwnb in cmn_fw_nbs(refr, targ, cdy):
+        fw_num = fw_count(refr + fwnb, cdy) * fw_count(targ + fwnb, cdy)
+        fw_den = fw_count(refr, cdy) * bw_count(fwnb, cdy)
+        fw_prob += fw_num / fw_den
+    return fw_prob
+
+def bw_gen_prob(refr, targ, cdy):
+    bw_prob = 0
+    for bwnb in cmn_bw_nbs(refr, targ, cdy):
+        bw_num = bw_count(bwnb + refr, cdy) * bw_count(bwnb + targ, cdy)
+        bw_den = bw_count(refr, cdy) * fw_count(bwnb, cdy)
+        bw_prob += bw_num / bw_den
+    return bw_prob
+
+def gen_prob_sep(refr, targ, cdy):
+    fw_num = 0
+    fw_den = 0
+    for fwnb in cmn_fw_nbs(refr, targ, cdy):
+        fw_num += fw_count(refr + fwnb, cdy) * fw_count(targ + fwnb, cdy)
+        fw_den += fw_count(refr + fwnb, cdy) * bw_count(fwnb, cdy)
+    fw_prob = fw_num / fw_den
+    bw_num = 0
+    bw_den = 0
+    for bwnb in cmn_bw_nbs(refr, targ, cdy):
+        bw_num += bw_count(bwnb + refr, cdy) * bw_count(bwnb + targ, cdy)
+        bw_den += bw_count(bwnb + refr, cdy) * fw_count(bwnb, cdy)
+    bw_prob = bw_num / bw_den
+    return fw_prob * bw_prob
+
+def sims(gram, cdy):
+    
 
 # -----
 # Analogical path finding algorithms
 # -----
+
+def mx_paths(ctxts, goals, cdy):
+    m_pairs = set()
+    ac_items = set()
+    ag_items = set()
+    for ctxt in ctxts:
+        ac_items.add(ctxt['item'])
+        for goal in goals:
+            ag_items.add(goal['item'])
+            for anl_goal in fw_nbs(ctxt['item'], cdy, len(goal['item'])):
+                for anl_ctxt in cmn_bw_nbs(anl_goal, goal['item'], cdy, len(ctxt['item'])):
+                    ac_items.add(anl_ctxt)
+                    ag_items.add(anl_goal)
+                    m_pairs.add((anl_ctxt, anl_goal))
+                    #'''
+                    m_pairs.add((ctxt['item'], anl_goal))
+                    m_pairs.add((anl_ctxt, goal['item']))
+                    #'''
+    fw_acs = defaultdict(float)
+    bw_acs = defaultdict(float)
+    for ctxt in ctxts:
+        for anl_ctxt in ac_items:
+            try:
+                fw_acs[anl_ctxt] += ctxt['weight'] * fw_gen_prob(anl_ctxt, ctxt['item'], cdy)
+            except:
+                pass
+            try:
+                bw_acs[anl_ctxt] += ctxt['weight'] * bw_gen_prob(anl_ctxt, ctxt['item'], cdy)
+            except:
+                pass
+    fw_ags = defaultdict(float)
+    bw_ags = defaultdict(float)
+    for goal in goals:
+        for anl_goal in ag_items:
+            try:
+                fw_ags[anl_goal] += goal['weight'] * fw_gen_prob(anl_goal, goal['item'], cdy)
+            except:
+                pass
+            try:
+                bw_ags[anl_goal] += goal['weight'] * bw_gen_prob(anl_goal, goal['item'], cdy)
+            except:
+                pass
+    anls = []
+    for anl_ctxt, anl_goal in m_pairs:
+        ac_prob = fw_acs[anl_ctxt] * bw_acs[anl_ctxt]
+        ag_prob = fw_ags[anl_goal] * bw_ags[anl_goal]
+        anls.append((anl_ctxt + anl_goal, ac_prob * ag_prob))
+    '''
+    for anl_ctxt, anl_goal in m_pairs:
+        ctxt_gen =   prob_fw(anl_ctxt, anl_goal, cdy) \
+                   * prob_bw(ctxt['item'], anl_goal, cdy)
+        acr_items[anl_ctxt] += ctxt_gen * ctxt['weight']
+        goal_gen =   prob_bw(anl_ctxt, anl_goal, cdy) \
+                   * prob_fw(anl_ctxt, goal['item'], cdy)
+        agl_items[anl_goal] += goal_gen * goal['weight']
+    acl_items = defaultdict(float)
+    for anl_ctxt in acr_items:
+        if anl_ctxt[0][:2] == '<s':
+            continue
+        for ctxt in ctxts:
+            for lft_envr in cmn_bw_nbs(ctxt['item'], anl_ctxt, cdy):
+                ll_prob =   prob_fw(lft_envr, ctxt['item'], cdy) \
+                          * prob_bw(lft_envr, anl_ctxt, cdy)
+                acl_items[anl_ctxt] += ll_prob * ctxt['weight']
+    agr_items = defaultdict(float)
+    for anl_goal in agl_items:
+        if anl_goal[-1][:2] == '</':
+            continue
+        for goal in goals:
+            for rgt_envr in cmn_fw_nbs(anl_goal, goal['item'], cdy):
+                rr_prob =   prob_fw(anl_goal, rgt_envr, cdy) \
+                          * prob_bw(goal['item'], rgt_envr, cdy)
+                agr_items[anl_goal] += rr_prob * goal['weight']
+    anls = []
+    for anl_ctxt, anl_goal in m_pairs:
+        if anl_ctxt[0][:2] == '<s' or anl_goal[-1][:2] == '</':
+            continue
+        ctxt_prob = acr_items[anl_ctxt] * acl_items[anl_ctxt]
+        goal_prob = agl_items[anl_goal] * agl_items[anl_goal]
+        jt_prob = ctxt_prob * goal_prob
+        anls.append((anl_ctxt + anl_goal, jt_prob))
+    '''
+    return sorted(anls, key=lambda x: x[1], reverse=True)
 
 def aps(ctxt, goal, cdy, n=float('inf')):
     rl_grams = defaultdict(float)
@@ -299,9 +497,9 @@ def aps2_split(ctxt, goal, cdy, n=float('inf')):
         r_grams[r_gram] = nb_gen(r_gram, goal, cdy)
     anls = defaultdict(float)
     for anl_ctxt, anl_goal in m_set:
-        l_prob = l_grams[anl_ctxt]# * prob_jt(anl_ctxt, goal, cdy)
-        r_prob = r_grams[anl_goal]# * prob_jt(ctxt, anl_goal, cdy)
-        j_prob = r_prob * l_prob# * prob_jt(anl_ctxt, anl_goal, cdy)
+        l_prob = l_grams[anl_ctxt]
+        r_prob = r_grams[anl_goal]
+        j_prob = r_prob * l_prob
         if len(ctxt + anl_goal)     < n:
             anls[(ctxt, anl_goal)]     += r_prob
         if len(anl_ctxt + goal)     < n:
@@ -431,25 +629,19 @@ def rec_parse_split(gram, cdy, anl_dy=None, n=float('inf')):
     splits = ((gram[:i], gram[i:]) for i in range(1,len(gram)))
     anls = []
     for ctxt, goal in splits:
-        split_dy = defaultdict(float)
         # Recursive calls
-        rec_ctxts = rec_parse_split(ctxt, cdy, anl_dy, n)[ctxt]
-        rec_goals = rec_parse_split(goal, cdy, anl_dy, n)[goal]
+        rec_ctxts = rec_parse_split(ctxt, cdy, anl_dy, n)[ctxt][:10]
+        rec_goals = rec_parse_split(goal, cdy, anl_dy, n)[goal][:10]
         for rec_ctxt in rec_ctxts:
             for rec_goal in rec_goals:
                 tree_type = (rec_ctxt['tree type'], rec_goal['tree type'])
-                abts = []
                 paths = aps2_split(rec_ctxt['path'], rec_goal['path'], cdy, n)[:30]
                 for path, path_score in paths:
                     score = path_score * rec_ctxt['score'] * rec_goal['score']
                     tree = ((' '.join(path[0]), rec_ctxt['tree']),
                             (' '.join(path[1]), rec_goal['tree']))
-                    abts.append({'path': path[0] + path[1], 'score': score,
+                    anls.append({'path': path[0] + path[1], 'score': score,
                                  'tree': tree, 'tree type': tree_type})
-                abts.sort(reverse=True, key=lambda x: x['score'])
-                anls.append({'tree type': abts})
-    anls = sorted(anls, reverse=True,
-                  key=lambda x: sum(item['score'] for item in x['tree type']))[0]['tree type'][:10]
     anls.sort(reverse=True, key=lambda x: x['score'])
     anl_dy[gram] = anls
     return anl_dy
@@ -515,7 +707,7 @@ def rec_parse_tree2(tree, cdy, anl_dy=None, n=float('inf')):
     return anl_dy
 
 # -----
-# Visualising analyses
+# Functions for visualising analyses
 # -----
 
 def ppt(tree, coords=(), ones_out=[]):
