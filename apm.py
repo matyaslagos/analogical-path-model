170ec8df18e0b100d78edf97a4637c09be15a165
Matyas Lagos <lagosmatyas@gmail.com>
2025-01-21 16:26:49 +0100

    Add shared context finder method

--- apm.py
+++ apm.py
@@ -61,8 +61,7 @@ class FreqTrie:
 class DistrNode(FreqNode):
     def __init__(self, label):
         super().__init__(label)
-        self.filler_finder = FreqTrie() # Left-to-right left contexts
-        self.context_finder = FreqTrie() # Right-to-left left contexts
+        self.finder = FreqTrie() # Left-to-right left contexts
 
 class DistrTrie:
     def __init__(self):
@@ -70,26 +69,41 @@ class DistrTrie:
     
     # Record distribution information about a sentence
     def insert_distr(self, sentence):
+        """Record all contexts and fillers of `sentence` into trie.
+        
+        Arguments:
+            - sentence (tuple of strings): e.g. ('the', 'king', 'was', 'here')
+        
+        Effect:
+            For each prefix--suffix pair of `sentence`, records the suffix as
+            a branch, and for each word in this branch, records all suffixes
+            of the prefix as branches.
+            The resulting trie structure can be used to look up shared fillers
+            of two contexts or shared contexts of two fillers.
+            In the former case:
+            - main branches act as fillers and right contexts, and
+            - finder branches act as left contexts.
+            In the latter case:
+            - main branches act as right contexts, and
+            - finder branches act as left contexts and fillers.
+        """
         context_pairs = ((sentence[:i], sentence[i:]) for i in range(len(sentence)))
         for left_context, right_context in context_pairs:
+            left_context_suffixes = [left_context[j:] for j in range(len(left_context))]
             current_node = self.root
+            for left_context_suffix in left_context_suffixes:
+                current_node.finder.root.get_or_make_branch(left_context_suffix)
             for word in right_context:
                 current_node = current_node.get_or_make_child(word)
                 current_node.count += 1
-                """
-                # Record right-to-left left context
-                context_finder_node = current_node.context_finder.root
-                context_finder_node.get_or_make_branch(tuple(reversed(left_context)), count_each_word=True)
-                """
                 # Record all suffixes of left-to-right context
-                filler_finder_node = current_node.filler_finder.root
-                left_context_suffixes = (left_context[j:] for j in range(len(left_context)))
+                finder_node = current_node.finder.root
                 for left_context_suffix in left_context_suffixes:
-                    filler_finder_node.get_or_make_branch(left_context_suffix)
+                    finder_node.get_or_make_branch(left_context_suffix)
     
-    # Yield all shared fillers of two contexts
+    # Yield each shared filler of two contexts
     def shared_fillers(self, context1, context2):
-        """Yield all shared fillers of `context1` and `context2`.
+        """Yield each shared filler of `context1` and `context2`.
         
         Arguments:
             - context1 (string): e.g. 'a _ garden'
@@ -100,14 +114,14 @@ class DistrTrie:
         """
         context_node1 = self.get_context_node(context1)
         context_node2 = self.get_context_node(context2)
-        return self.shared_fillers_aux(context_node1, context_node2)
+        return self.shared_branches(context_node1, context_node2)
     
     # Return the filler finder trie of a context
     def get_context_node(self, context):
         """Return the filler finder trie of `context`.
         
         Argument:
-            - context (tuple of strings): e.g. ('a', '_', 'king')
+            - context (string): e.g. 'a _ king'
         
         Returns:
             - DistrNode: the node that is the root of the trie of the fillers
@@ -115,6 +129,7 @@ class DistrTrie:
         """
         left_context, right_context = map(lambda x: x.strip().split(), context.split('_'))
         context_node = self.root
+        # Go to node of right context
         for i, word in enumerate(right_context):
             try:
                 context_node = context_node.children[word]
@@ -123,7 +138,8 @@ class DistrTrie:
                 raise KeyError(
                     f'Context \"{context}\" not found (failed at \"{failed_part}\")'
                     )
-        context_node = context_node.filler_finder.root
+        # Within the filler finder of right context, go to node of left context
+        context_node = context_node.finder.root
         for i, word in enumerate(left_context):
             try:
                 context_node = context_node.children[word]
@@ -134,26 +150,92 @@ class DistrTrie:
                     )
         return context_node
     
-    # Recursively find all shared fillers from two context nodes
-    def shared_fillers_aux(self, context_node1, context_node2, path=[]):
-        """Yield all shared fillers of `context_node1` and `context_node2`.
+    # Recursively yield each shared filler of two context nodes
+    def shared_branches(self, distr_node1, distr_node2, path=[]):
+        """Yield each shared branch of `distr_node1` and `distr_node2`.
         
         Arguments:
-            - context_node1 (DistrNode): filler trie of a context
-            - context_node2 (DistrNode): filler trie of another context
+            - distr_node1 (DistrNode): root of a subtrie
+            - distr_node2 (DistrNode): root of another subtrie
         
         Yields:
-            - string: filler that is shared by the two input contexts
+            - string: branch that is shared by the two input subtries
         """
-        for child in context_node1.children:
-            if child in context_node2.children:
+        for child in distr_node1.children:
+            if child in distr_node2.children:
                 new_path = path + [child]
-                child_node1 = context_node1.children[child]
-                child_node2 = context_node2.children[child]
-                if child_node1.count * child_node2.count > 0:
+                child_node1 = distr_node1.children[child]
+                child_node2 = distr_node2.children[child]
+                if child_node1.count > 0 and child_node2.count > 0:
                     yield ' '.join(new_path)
-                yield from self.shared_fillers_aux(child_node1, child_node2, new_path)
+                yield from self.shared_branches(child_node1, child_node2, new_path)
         
-    # Generator of all shared contexts of two fillers
+    # Yield each shared context of two fillers
     def shared_contexts(self, filler1, filler2):
-        pass
+        """Yield each shared context of `filler1` and `filler2`.
+        
+        Arguments:
+            - filler1 (string): e.g. 'the king'
+            - filler2 (string): e.g. 'this garden'
+        
+        Returns:
+            - generator (of strings): e.g. ('visited _ today', 'i saw _', ...)
+        """
+        filler_node1 = self.get_filler_node(filler1)
+        filler_node2 = self.get_filler_node(filler2)
+        return self.shared_contexts_aux(filler_node1, filler_node2)
+    
+    # Return the node of a filler
+    def get_filler_node(self, filler):
+        """Return the context finder node of `filler`.
+        
+        Argument:
+            - filler (string): e.g. 'the nice king'
+        
+        Returns:
+            - DistrNode: the main trie node of `filler`
+        """
+        filler_words_list = filler.split()
+        filler_node = self.root
+        for i, word in enumerate(filler_words_list):
+            try:
+                filler_node = filler_node.children[word]
+            except KeyError:
+                failed_part = ' '.join(filler_list[:i+1]) + ' ...'
+                raise KeyError(
+                    f'Filler \"{filler}\" not found (failed at \"{failed_part}\")'
+                    )
+        return filler_node
+    
+    # Recursively yield each shared context of two filler nodes
+    def shared_contexts_aux(self, filler_node1, filler_node2, shared_right_context=[]):
+        """Yield each shared context of `filler_node1` and `filler_node2`.
+        
+        Arguments:
+            - filler_node1 (DistrNode): node where children are considered to
+              be trie of right contexts and .finder trie is considered to
+              be trie of left contexts
+            - filler_node1 (DistrNode): as filler_node1
+        
+        Yields:
+            - string: shared context of fillers, e.g. 'visited _ today'
+        """
+        # Find all shared left contexts within the current shared right context
+        left_contexts1 = filler_node1.finder.root
+        left_contexts2 = filler_node2.finder.root
+        shared_left_contexts = self.shared_branches(left_contexts1, left_contexts2)
+        for shared_left_context in shared_left_contexts:
+            yield (shared_left_context + ' _ ' + ' '.join(shared_right_context)).strip()
+        # Recursive traversal of each shared child of the fillers, to cover all
+        # shared right contexts
+        for child in filler_node1.children:
+            if child in filler_node2.children:
+                new_shared_right_context = shared_right_context + [child]
+                # Yield newly found shared right context by itself
+                yield '_ ' + ' '.join(new_shared_right_context)
+                # Recursive call on new shared right context and new child
+                # nodes, to find the shared left contexts within this new right
+                # context
+                child_node1 = filler_node1.children[child]
+                child_node2 = filler_node2.children[child]
+                yield from self.shared_contexts_aux(child_node1, child_node2, new_shared_right_context)
